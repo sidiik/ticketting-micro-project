e8dffb96aa661c27acb24ffd96413fba1ed0f051
sidiik <sidiikpro@gmail>
2023-02-08 12:19:15 +0300

    initial commit

---
+++ auth/src/index.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
